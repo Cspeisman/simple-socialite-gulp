9d272606e294af7f505cfe68778ec0e39a75ace0
Cspeisman <Cspeisman@gmail.com>
2015-01-24 13:12:34 -0500

    clone task to clone libraries

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,22 @@
 var gulp = require('gulp'),
     clean = require('gulp-clean'),
-    download = require('gulp-download');
+    fs = require('fs'),
+    git = require('gulp-git'),
+    config = require('yaml-config')
+    settings = config.readConfig('./settings.yml');
+
+gulp.task('clone', ['clean'], function(){
+  fs.mkdirSync('./components');
+  settings.libraries.forEach(function(repo){
+    git.clone('https://github.com/' + repo, {cwd: './components'}, function (err) {
+      if (err) throw err;
+    })
+  })
+});
 
 gulp.task('clean', function(){
   return gulp.src(['./build', './components'], {read: false})
   .pipe(clean());
 });
 
-gulp.task('default', function(){
-  console.log("hello world");
-});
+gulp.task('default', ['clean', 'clone']);
