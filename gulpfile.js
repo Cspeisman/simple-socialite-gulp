15823a31e466de06f6ff2040bc105d75ff338b43
Corey Speisman <Cspeisman@gmail.com>
2015-02-01 16:25:37 -0500

    it finally works

--- gulpfile.js
+++ gulpfile.js
@@ -9,11 +9,11 @@ var gulp = require('gulp'),
     _ = require('underscore'),
     coffee = require('gulp-coffee'),
     uglify = require('gulp-uglify'),
+    copysync = require('copysync'),
     rename = require('gulp-rename');
 
 
-
-gulp.task('coffee', ['clone'], function(){
+gulp.task('coffee', ['verify'], function(){
   return gulp.src('./src/*.coffee')
   .pipe(coffee({bare: true}))
   .pipe(gulp.dest('./build/'))
@@ -30,31 +30,35 @@ function addSettings(){
   fs.writeFileSync(file, js);
 }
 
-// function verifyExtensions(){
-//   tmp_arr = []
-//   extensions = args.e.split(" ")  
-//   files = glob.sync('./components/**/*');
-
-//   _.each(extensions, function(ext){
-//     _.each(files, function(file){
-//       re = new RegExp(ext)
-//       if(file.match(re)){
-//         console.log(file)
-//         tmp_arr.push(ext)
-//       }
-//     });
-//   });
-//   console.log(tmp_arr)
-// }
+gulp.task('pack', ['coffee'], function(){
+  js = fs.readFileSync("./components/socialite/socialite.js").toString() + "\n";
+  paths = glob.sync("./.tmp/*.js");
+  _.each(paths, function(file){
+    js += fs.readFileSync(file).toString() + "\n"
+  });
+  js += fs.readFileSync("./build/simple-socialite.js").toString() + "\n"
+  return fs.writeFileSync("./build/simple-socialite-pack.js", js);
+});
 
 gulp.task('verify', ['clone'], function(){
+  extensions_path = [];
+  extensions = args.e.split(" ");
+  fs.mkdirSync('./.tmp');
   files = glob.sync('./components/**/*');
-  console.log('called back!')
-  console.log(files)
+
+  _.each(extensions, function(extension){
+    extensions_path = _.filter(files, function(path){
+      return path.match(new RegExp('socialite.' + extension + '.js'));    
+    });
+
+    if(extensions_path.length){
+      copysync(extensions_path[0], './.tmp');
+    }
+  });
+
 });
 
 gulp.task('clone', ['clean'], function(cb){
-  console.log('cleaned');
   fs.mkdirSync('./components');
   _.each(settings.libraries, function(repo){
     git.clone('https://github.com/' + repo, {cwd: './components'}, function (err) {
@@ -68,11 +72,11 @@ gulp.task('clone', ['clean'], function(cb){
 
 
 gulp.task('clean', function(){
-  return gulp.src(['./build', './components', './build'], {read: false})
+  return gulp.src(['./build', './components', './build', './.tmp'], {read: false})
   .pipe(clean());
 });
 
-gulp.task('default', ['clean', 'clone', 'verify'], function(){
+gulp.task('default', ['clean', 'clone', 'verify', 'coffee', 'pack'], function(){
   console.log('finished')
 });
 
